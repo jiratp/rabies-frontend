3bf9bfb2e78c07271e0e5b1200a6aa3eb8c9883c
Sirintra1 <sirintrawannakeaw@gmail.com>
2018-11-09 23:19:17 +0700

    Add component, core and providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule
