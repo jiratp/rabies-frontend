d787b325bbfd218c718d9bd7cb169ff153e333fc
Sirintra1 <sirintrawannakeaw@gmail.com>
2018-11-11 19:50:22 +0700

    update home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import { PagesComponent } from './pages/pages.component';
 import { Routes, RouterModule } from '@angular/router';
 import { PagesModule } from './pages/pages.module';
+import { FooterComponent } from './components/footer/footer.component';
 
 const appRoutes: Routes = [
   {
@@ -19,7 +16,8 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
